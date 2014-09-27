490cb405ab9cf1b252fef96a7357d952b90664d6
Matthew MacLean <matthewtmaclean@gmail.com>
2014-09-26 23:22:49 -0400

    Initial commit

---
+++ server_workspace/ChatServer/ChatServer/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+import views
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ChatServer.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^test/', views.test_page),
+)
